8c6b721bbb609eb35b59ebdc09edee0c92f62e08
Prelang Builder <builder@prelang.com>
2017-03-04 16:30:55 +0000

    Sets ActiveAdmin comment registration name as 'AdminComment' to prevent naming conflicts

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -283,4 +283,5 @@ ActiveAdmin.setup do |config|
   # You can inherit it with own class and inject it for all resources
   #
   # config.order_clause = MyOrderClause
+  config.comments_registration_name = "AdminComment"
 end
